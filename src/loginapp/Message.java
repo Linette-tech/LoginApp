6ca942a22bfcc1d94c834c44cf92a9009978c604
Linette-tech <st10457818@rcconnect.edu.za>
2026-05-25 19:16:00 +0200

    Update Message.java
    Created the constructor

--- src/loginapp/Message.java
+++ src/loginapp/Message.java
@@ -8,4 +8,13 @@ public class Message {
     private String message;
     private String messageHash;
 
+    public Message(String messageID, int numMessagesSent, String recipient, String message) {
+
+        this.messageID = messageID;
+        this.numMessagesSent = numMessagesSent;
+        this.recipient = recipient;
+        this.message = message;
+
+    }
+    
 }
