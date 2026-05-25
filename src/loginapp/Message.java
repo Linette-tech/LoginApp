dad4da4b187e13ffb1bc5fa3d589c8c047081f82
Linette-tech <st10457818@rcconnect.edu.za>
2026-05-25 19:10:09 +0200

    Create Message.java
    created messaging Java

---
+++ src/loginapp/Message.java
@@ -0,0 +1,11 @@
+package loginapp;
+
+public class Message {
+
+    private String messageID;
+    private int numMessagesSent;
+    private String recipient;
+    private String message;
+    private String messageHash;
+
+}
