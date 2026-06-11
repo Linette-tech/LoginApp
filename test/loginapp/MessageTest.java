ac34f864157e549b25f5f7f9b4664b9d9673ad31
Linette-tech <st10457818@rcconnect.edu.za>
2026-06-11 23:21:53 +0200

    Added part 3 JUnit tests

--- test/loginapp/MessageTest.java
+++ test/loginapp/MessageTest.java
@@ -71,3 +71,54 @@ public class MessageTest {
     }
 
 }
+
+@Test
+public void testDisplayLongestMessage() {
+
+    Message.addMessage(
+            "1111111111",
+            "11:1:TESTMESSAGE",
+            "+27838884567",
+            "Where are you? You are late! I have asked you to be on time.",
+            "Stored");
+
+    String expected =
+            "Where are you? You are late! I have asked you to be on time.";
+
+    assertEquals(expected, Message.displayLongestMessage());
+    
+}
+
+@Test
+public void testSearchByRecipient() {
+
+    Message.addMessage(
+            "2222222222",
+            "22:1:TESTMESSAGE",
+            "+27838884567",
+            "Ok, I am leaving without you.",
+            "Stored");
+
+    String result =
+            Message.searchByRecipient("+27838884567");
+
+    assertTrue(result.contains("Ok, I am leaving without you."));
+    
+}
+
+@Test
+public void testDeleteByHash() {
+
+    Message.addMessage(
+            "3333333333",
+            "33:1:TESTMESSAGE",
+            "+27838884567",
+            "Delete me",
+            "Stored");
+
+    String result =
+            Message.deleteByHash("33:1:TESTMESSAGE");
+
+    assertTrue(result.contains("successfully deleted"));
+    
+}
